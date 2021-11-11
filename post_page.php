b8a4519e049fff7e8ba587f6b42fc320f864199c
Elanaly <69584940+Elanaly@users.noreply.github.com>
2021-11-11 12:24:52 +0900

    dev test

--- post_page.php
+++ post_page.php
@@ -1,1 +1,20 @@
-echo("Hello World");
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PostPage</title>
+</head>
+<body>
+    <h1>titleを入力してください</h1>
+    <input type="text" id="title" name="title" required minlength="1" maxlength="40">
+    <h1>本文を入力して下さい</h1>
+    <input type="text" id="content" name="content" required>
+    <br>
+    <input type="submit">
+    <?php
+    //echo ("Hello World");
+    ?>
+</body>
+</html>
+
