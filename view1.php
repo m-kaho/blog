763bdf0624b545e50301f618f5ef5873f63520d7
m-kaho <92501154+m-kaho@users.noreply.github.com>
2021-11-28 15:26:51 +0900

    Update view1.php
    プログラム部分ほぼ完成

--- view1.php
+++ view1.php
@@ -11,19 +11,21 @@ try {
     $dbh = new PDO('mysql:host=localhost;dbname=blog', $user, $pass);
     $dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
     //プリペアドステートメント
-    $stmt = $dbh->prepare('select * from blog order by post_id limit ?, ?');
+    $stmt = $dbh->prepare('select * from blog where private=1 order by post_id limit ?, ?');
     $stmt->execute(array($pageNum * $by, $by));
     $result = $stmt->fetchAll();
-    $dbh =null;
+
+    $count = $dbh->query('select count(post_id) from blog where private=1' );
+    $cnt = $count->fetch(PDO::FETCH_NUM);
+    $dbh = null;
+
 } catch (PDOException $e) {
     print "エラー発生：".$e->getMessage()."</br>";
     die();
 }
 
-
-$id_sum = count(["post_id"]); //総数
+$id_sum = $cnt[0]; //総数
 $max = ceil($id_sum / $by); //ページの最大値
-echo $id_sum;
 
 if (!isset($_GET['page_num'])) {
     $page = 1;
@@ -44,31 +46,27 @@ if (!isset($_GET['page_num'])) {
 </head>
 <body>
     <h1><?= $user ?>のブログ</h1>
-    <button>ログイン</button>
+    <button onclick="location.href='login.php'">ログイン</button>
     <?php foreach($result as $content): ?>
     <div>
+        <?php if ($content["private"] == 1): ?>
         <h2><a href="view2.php?post_id=<?= $content["post_id"] ?>"><?= $content["title"] ?></a></h2>
         <p>投稿日:<?= $content["post_date"] ?>,最終更新日:<?=$content["update_date"] ?></p>
         <p><?= $content["content"] ?></p>
+        <?php endif; ?>
     </div>
     <?php endforeach; ?>
     
-    <?php  if ($page > 1): ?>
+    <?php  if ($page > 1 && $page!=$max): ?>
     <a href="view1.php?page_num=<?php echo ($page-1); ?>">前へ</a>
     <a href="view1.php?page_num=<?php echo ($page+1); ?>">次へ</a>
     <?php endif; ?>
-    <?php  if ($page < $max): ?>
+    <?php  if ($page == $max): ?>
     <a href="view1.php?page_num=<?php echo ($page-1); ?>">前へ</a>
     <?php endif; ?>
     <?php  if ($page == 1): ?>
     <a href="view1.php?page_num=<?php echo ($page+1); ?>">次へ</a>
     <?php endif; ?>
 
-    <!-- <?php for ($x=1; $x <= $max ; $x++) { ?>
-	<a href="view1.php?page_num=<?php echo $x ?>"><?php echo $x; ?></a>
-    <?php } ?> -->
-
-    <!-- <a href="view1.php?page_num=2">2</a> -->
 </body>
 </html>
-
