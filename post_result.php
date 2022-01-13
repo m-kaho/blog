510080968684fc22429ffc2055793d63afa5f235
Elanaly <69584940+Elanaly@users.noreply.github.com>
2022-01-13 13:51:06 +0900

    完成

--- post_result.php
+++ post_result.php
@@ -5,6 +5,7 @@
     if(isset($_SESSION["isLogin"])){
         $isLogin = $_SESSION["isLogin"];
         $user = $_SESSION["user"];
+        $sUserid = $_SESSION["userid"];
         //ログインしているか確かめる
         if($isLogin == False){//ログインできていない場合
             header( "Location: login.php" ) ;
@@ -35,22 +36,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PostResult</title>
 </head>
 <body>
-    <h2>以下の内容で投稿しました</h2>
-    <h3>タイトル</h3>
-    <p><?=$title?></p>
-    <h3>本文</h3>
-    <p><?=$content?></p>
-    <h3>公開状態</h3>
-    <?php
-    if($private == 1){
-        echo("<p>公開</p>");
-    }else{
-        echo("<p>非公開</p>"); 
-    }
-    ?>
-    <a href="post_page.php">戻る</a>
+    <div class="container">
+        <h2>投稿できました</h2>
+        <a href="view1.php?page_num=1&userid=".$sUserid>投稿を見に行く</a>
+        <a href="post_page.php">戻る</a>
+    </div>
+
 </body>
 </html>
