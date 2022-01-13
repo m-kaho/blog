c017e6272d1e68657478056e1426559c0b2ab459
Elanaly <69584940+Elanaly@users.noreply.github.com>
2022-01-13 13:46:38 +0900

    session追加後

--- post_page.php
+++ post_page.php
@@ -1,20 +1,14 @@
 <?php
     session_start();
 ?>
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="post_page.css">
-    <title>PostPage</title>
-</head>
-<body>
+<link rel="stylesheet" href="post.css">
     <?php
         //セッション変数を調べる
         if(isset($_SESSION["isLogin"])){
             $isLogin = $_SESSION["isLogin"];
             $user = $_SESSION["user"];
+            
+
             //ログインしているか確かめる
             if($isLogin == False){//ログインできていない場合
                 header( "Location: login.php" ) ;
@@ -24,23 +18,42 @@
         }
         
     ?>
-        <h1>投稿画面</h1>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PostPage</title>
+</head>
+<body>
+    <header>
+        <img src="logo/logo2.png" class="logoImage">
+    </header>
+    <div id='contents'>
         <form id="post-form" method='post' action='post_page.php'>
-        <h2>タイトルを入力してください</h2>
-        <input type='text' id='title' name='title'>
-        <h2>本文を入力して下さい</h2>
-        <input type='text' id='content' name='content'>
-        <br>
-        <input type='radio' name='private' value='1' checked/>公開
-        <input type='radio' name='private' value='2'/>非公開
-        </br>
+            <div class="post">
+                <input type='text' id='title' name='title' placeholder="タイトルを入力してください"><br>
+                <textarea name="content" id="content" cols="120" rows="10" placeholder="内容を入力してください"></textarea>
+            </div>
+
+            <br>
+            <input type='radio' name='private' value='1' checked/>公開
+            <input type='radio' name='private' value='2'/>非公開
+            </br>
         </form>
-        <button id='postBtn'>投稿</button>
         <div id='alert'>
-        <p id='emptyTitle' class="hide">タイトルが空です</p>
-        <p id='lengthTitle' class="hide">タイトルを40文字以下にしてください</p>
-        <p id='emptyContent' class="hide">本文が空です</p>
+            <p id='emptyTitle' class="hide">タイトルが空です</p>
+            <p id='lengthTitle' class="hide">タイトルを40文字以下にしてください</p>
+            <p id='emptyContent' class="hide">本文が空です</p>
+        </div>
+        <div>
+        <button id='postBtn'>投稿</button>
+        </div>
+
+
     </div>
+    
+
         <script>
             const post = document.querySelector('#postBtn');
             post.addEventListener('click', ()=> {
@@ -81,11 +94,13 @@
             $title = $_POST["title"];
             $content = $_POST["content"];
             $private = $_POST["private"];
+            $userid = $_SESSION["userid"];
+            //echo($userid);
             //sql文
             $sql =  'INSERT INTO
-                post(title, content, private)
+                post(title, content, private, userid)
             VALUES
-                (:title, :content, :private)';
+                (:title, :content, :private, :userid)';
             //userID
             $user = "postuser";
             //PassWord
@@ -101,6 +116,7 @@
                 $stmt->bindValue(':title',$title, PDO::PARAM_STR);
                 $stmt->bindValue(':content',$content, PDO::PARAM_STR);
                 $stmt->bindValue(':private',$private, PDO::PARAM_INT);
+                $stmt->bindValue(':userid',$userid, PDO::PARAM_INT);
                 //DBに書き込み
                 $stmt->execute();
 
@@ -115,14 +131,9 @@
             }   
 
     }
-
-
-
-
-        
-        
-        
     ?>
-
+    <!-- <footer>
+        <p id="copy">&copy;beginner's</p>
+    </footer> -->
 </body>
 </html>
