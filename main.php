c4f6e0c28bd5aecacd159347b6efbc0fff5105b8
Elanaly <69584940+Elanaly@users.noreply.github.com>
2022-01-27 01:56:57 +0900

    tochuu

--- main.php
+++ main.php
@@ -1,8 +1,9 @@
 <?php
     session_start();
 ?>
-
+<link rel="stylesheet" href="main.css">
 <?php
+require('headerLogin.php');
 //データベースに接続
 function dbConnect(){
     $user = 'postuser';
@@ -61,6 +62,18 @@ function recentPosts(){
     $dbh = null;
     return $data;
 }
+//登録されているユーザーが何人いるか調べる
+function searchUser(){
+    $dbh = dbConnect();
+    $sql = 'SELECT count(userid) FROM user';
+    $dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+    $stmt = $dbh->prepare($sql);
+    $stmt->execute();
+    $data = $stmt->fetch();
+    var_dump($data['count(userid)']);
+    $dbh = null;
+    return $data['count(userid)'];
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -71,41 +84,64 @@ function recentPosts(){
     <title>メイン</title>
 </head>
 <body>
-    <h1>最近の投稿</h1>
-    <?php
-    //ループのカウント
-    $count = 1;
-    $result = recentPosts();
-    foreach($result as $data):?>
-        <h2><?=$count?>位</h2>
-        <h3><a href="view2.php?post_id=<?=$data['post_id']?>"><?=$data['title']?></a></h3>
+    <header>
+        <img src="logo/logo2.png" class="logoImage">
+        <nav>
+            <ul class="clearfix">
+                <a class="view1" href="view1.php?page_num=1&userid=<?php echo $sUserid; ?>">閲覧画面</a>
+                <?php
+                    echo($log);
+                ?>
+            </ul>
+        </nav>
+    </header>
+    <div class="main">
+        <h1>最近の投稿</h1>
         <?php
-        $user = searchPostID($data['userid']);//useridからユーザー情報を呼び出す
+        //ループのカウント
+        $count = 1;
+        $result = recentPosts();
         ?>
-        <a href="view1.php?userid=<?=$data['userid']?>&page_num=1"><?=$user['username']?></a><!-- ユーザーの投稿ページ -->
-        <p><?=$data['post_date']?></p>
+        <div class='recentPosts'>
         <?php
-        $count++;
-    endforeach;
+        foreach($result as $data):?>
+            <div class='box'>
+                <h2><?=$count?>位</h2>
+                <h3><a href="view2.php?post_id=<?=$data['post_id']?>"><?=mb_substr($data['title'],0,8).'...'?></a></h3>
+                <?php
+                $user = searchPostID($data['userid']);//useridからユーザー情報を呼び出す
+                ?>
+                <a href="view1.php?userid=<?=$data['userid']?>&page_num=1"><?=$user['username']?></a><!-- ユーザーの投稿ページ -->
+                <p class="post_date"><?=$data['post_date']?></p>
+            </div>
 
-    ?>
-    <h1>アクセス数</h1>
-    <?php
-    $result = pvPosts();
-    $count = 1;
-    //resultのデータすべてループ
-    foreach($result as $data)://もし見た目に内容が必要であれば$data['content']で表示できます?>
-        <h1><?=$count?>位</h1>
-        <h2><a href="view2.php?post_id=<?=$data['post_id']?>"><?=$data['title']?></a></h2><!-- 投稿内容に飛ぶリンク -->
-        <?php
-        $user = searchPostID($data['userid']);//useridからユーザー情報を呼び出す
+            <?php
+            $count++;
+        endforeach;
         ?>
-        <a href="view1.php?userid=<?=$data['userid']?>&page_num=1"><?=$user['username']?></a><!-- ユーザーの投稿ページ -->
-        <p><?=$data['post_date']?></p>
-    <?php
-    $count++; //ループのカウント
-    endforeach;
-    ?>
-    <h1>最近の投稿が多いユーザー</h1>
+        </div>
+        <h1>アクセス数</h1>
+        <div class="pvRanking">
+        <?php
+        $result = pvPosts();
+        $count = 1;
+        //resultのデータすべてループ
+        foreach($result as $data)://もし見た目に内容が必要であれば$data['content']で表示できます?>
+            <div class='box'>
+            <h2><?=$count?>位</h2>
+            <h3><a href="view2.php?post_id=<?=$data['post_id']?>"><?=mb_substr($data['title'],0,8).'...'?></a></h3><!-- 投稿内容に飛ぶリンク -->
+            <?php
+            $user = searchPostID($data['userid']);//useridからユーザー情報を呼び出す
+            ?>
+            <a href="view1.php?userid=<?=$data['userid']?>&page_num=1"><?=$user['username']?></a><!-- ユーザーの投稿ページ -->
+            <p><?=$data['pv']?>PV</p>
+            </div>
+        <?php
+        $count++; //ループのカウント
+        endforeach;
+        ?>
+        </div>
+    </div>
+
 </body>
 </html>
