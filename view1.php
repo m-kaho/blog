47ecbd7df74e7f62d124e39d4dc360de0beb3ea4
Elanaly <69584940+Elanaly@users.noreply.github.com>
2022-01-27 10:44:56 +0900

    henkou

--- view1.php
+++ view1.php
@@ -1,22 +1,49 @@
 <?php
+    session_start();
+?>
+<?php
 //phpinfo();
-$title = "ブログシステム";
+require('headerLogin.php');
+$title = "Mylog";
 $user = "postuser";
-$pass = "2021";
+$pass = "e2k2021";
 
 $pageNum = $_GET['page_num'] - 1;
-$by = 1;
+$by = 4;
 
+$userId = $_GET['userid'];
+function getUserData($userid){
+    try {
+        $user = "postuser";
+        $pass = "e2k2021";
+        $dbh = new PDO('mysql:host=localhost;dbname=blog', $user, $pass);
+        $dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+        $sql = 'SELECT username FROM user WHERE userid = :id';
+        $stmt = $dbh->prepare($sql);
+        $stmt->bindValue('id',(int)$userid,PDO::PARAM_INT);
+        $stmt->execute();
+        $username = $stmt->fetch();
+        $dbh = NULL;
+        // var_dump($username);
+        $dbh = null;
+        return $username;
+    } catch (PDOException $e) {
+        print "エラー発生：".$e->getMessage()."</br>";
+        die();
+    }
+}
 try {
     $dbh = new PDO('mysql:host=localhost;dbname=blog', $user, $pass);
     $dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
     //プリペアドステートメント
-    $stmt = $dbh->prepare('select * from blog where private=1 order by post_id limit ?, ?');
-    $stmt->execute(array($pageNum * $by, $by));
+    $stmt = $dbh->prepare('select post.* from post,user where post.private=1 and post.userid=? and post.userid=user.userid order by post.post_id limit ?, ?');
+    $stmt->execute(array($userId, $pageNum * $by, $by));
     $result = $stmt->fetchAll();
 
-    $count = $dbh->query('select count(post_id) from blog where private=1' );
+    $count = $dbh->prepare('select count(post.post_id) from post,user where post.private=1 and post.userid=? and post.userid=user.userid' );
+    $count->execute(array($userId));
     $cnt = $count->fetch(PDO::FETCH_NUM);
+
     $dbh = null;
 
 } catch (PDOException $e) {
@@ -42,34 +69,52 @@ if (!isset($_GET['page_num'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="view1.css" rel="stylesheet" type=text/css>
     <title><?= $title ?></title>
 </head>
 <body>
-    <h1><?= $user ?>のブログ</h1>
-    <button onclick="location.href='login.php'">ログイン</button>
+<header>
+    <img src="logo/logo2.png">
+    <nav>
+        <ul class="clearfix">
+            <a class="view1" href="main.php">メインページ</a>
+            <?php echo($log);?>
+        </ul>
+    </nav>
+</header>
+<div class=contents>
+    <?php
+        $username = getUserData($userId);
+        // var_dump($username);
+    ?>
+    <p class="page"><?= $username['username'] ?>さんのブログ</p>
     <?php foreach($result as $content): ?>
     <div>
-        <h2><a href="view2.php?post_id=<?= $content["post_id"] ?>"><?= $content["title"] ?></a></h2>
-        <p>投稿日:<?= $content["post_date"] ?>,最終更新日:<?=$content["update_date"] ?></p>
-        <p><?= $content["content"] ?></p>
+        <h2><a class="title" href="view2.php?post_id=<?= $content["post_id"] ?>"><?= $content["title"] ?></a></h2>
+        <p class="content"><?= mb_substr($content['content'],0,20).'…'; ?></p>
+        <p class="date">投稿日:<?= $content["post_date"] ?> 最終更新日:<?=$content["update_date"] ?></p>
     </div>
     <?php endforeach; ?>
-    
+
     <?php if($id_sum > $by): ?>
     <?php  if ($page > 1 && $page!=$max): ?>
-    <a href="view1.php?page_num=<?php echo ($page-1); ?>">前へ</a>
-    <a href="view1.php?page_num=<?php echo ($page+1); ?>">次へ</a>
+    <a href="view1.php?page_num=<?php echo ($page-1); ?>&userid=<?php echo $userId; ?>">前へ</a>
+    <a href="view1.php?page_num=<?php echo ($page+1); ?>&userid=<?php echo $userId; ?>">次へ</a>
     <?php endif; ?>
     <?php  if ($page == $max): ?>
-    <a href="view1.php?page_num=<?php echo ($page-1); ?>">前へ</a>
+    <a href="view1.php?page_num=<?php echo ($page-1); ?>&userid=<?php echo $userId; ?>">前へ</a>
     <?php endif; ?>
     <?php  if ($page == 1): ?>
-    <a href="view1.php?page_num=<?php echo ($page+1); ?>">次へ</a>
+    <a href="view1.php?page_num=<?php echo ($page+1); ?>&userid=<?php echo $userId; ?>">次へ</a>
     <?php endif; ?>
     <?php endif; ?>
     <br>
-    <?php 
-        echo '全'.$id_sum.'件' ;
-    ?>
+    <?php echo '全'.$id_sum.'件' ; ?>
+</div>
+<footer>
+<p id="copy">
+    &copy;beginner's
+</p>
+</footer>
 </body>
 </html>
