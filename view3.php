0cf2038a8b6c9425d4ec37db632c245954655907
m-kaho <92501154+m-kaho@users.noreply.github.com>
2022-01-27 10:52:18 +0900

    Update view3.php
    完成

--- view3.php
+++ view3.php
@@ -69,7 +69,7 @@ if (!isset($_GET['page_num'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="view3.css" rel="stylesheet" type=text/css>
+    <link href="view.css" rel="stylesheet" type=text/css>
     <title><?= $title ?></title>
 </head>
 <body>
